8d5eebbd8baa6dee34a77b4c7801bc7a5aac66f5
Brian Elliott <bdelliott@gmail.com>
2013-12-29 11:37:43 -0600

    Run any fixture under profiling via CLI

--- eprofile/cli.py
+++ eprofile/cli.py
@@ -3,6 +3,7 @@
 import eventlet
 eventlet.monkey_patch()
 
+import optparse
 import sys
 
 from eprofile import fixtures
@@ -10,9 +11,22 @@ from eprofile import trace
 
 
 def main():
+    parser = optparse.OptionParser()
+    opts, args = parser.parse_args()
 
+    if len(args) == 0:
+        func = fixtures.foo
+        args = [1]
+    else:
+        funcname = args.pop(0)
+        func = getattr(fixtures, funcname)
+
+    _exec(func, *args)
+
+
+def _exec(func, *args):
     prof = trace.Profiler()
-    prof.runcall(fixtures.foo)
+    prof.runcall(func, *args)
 
     print "-"*80
     print "Trace info:"
@@ -25,6 +39,7 @@ def main():
         for call in thread.calls:
             call.pretty()
 
+    print "-" *80
+    prof.print_stats()
 
-if __name__ == '__main__':
-    main()
+
